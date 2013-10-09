2175ae737c6e27acb0fe587f3c17cf4cbe8f26de
valdergallo <valdergallo@gmail.com>
2013-10-09 20:41:34 -0300

    fixed

--- runserver.py
+++ runserver.py
@@ -1,28 +1,36 @@
+# coding=utf-8
 from bottle import route, run, install, template, error, static_file
 from bottle_sqlite import SQLitePlugin
 
+# carrega o banco de dados pelo plugin do Bottle
 install(SQLitePlugin(dbfile='site.db'))
 
 
 @error(404)
 def error404(error):
+    # pagina de erros
     return 'Nothing here, sorry'
 
 
 @route('/static/<filename:path>')
 def send_static(filename):
+    # pagina de arquivos staticos: css, img ...
     return static_file(filename, root='./static/')
 
 
 @route('/')
-def home(db, post_id=None):
-    if not post_id:
+def home(db, vaga=None):
+    if not vaga:
+        # mostra todas as vagas
         c = db.execute('SELECT * FROM vagas')
     else:
-        c = db.execute('SELECT * FROM vagas WHERE id = ?', (post_id,))
+        # mostra apenas uma vaga em especial
+        c = db.execute('SELECT * FROM vagas WHERE vaga = ?', (vaga,))
 
+    # transforma o conteudo SQL em uma lista de dados
     rows = c.fetchall()
 
+    # envia os dados para o template
     return template('vagas', vagas=rows)
 
 run(host='0.0.0.0', port=8080, debug=True)
--- socket_server.py
+++ socket_server.py
@@ -1,25 +1,47 @@
+# coding=utf-8
 import serial
 import sqlite3
 import time
 
+# carrega o banco de dados SQLite
 con = sqlite3.connect('site.db')
 
-# ser = serial.Serial('COM1', 9600)
-ser = serial.Serial('/dev/tty.Bluetooth-Serial-1', 9600)
+# conecta com a serial
+ser = serial.Serial('COM4', 9600)
+# ser = serial.Serial('/dev/tty.Bluetooth-Serial-1', 9600)
 
 
 def update_content(con, line_value):
-    vagas = line_value.split(',')
+    # remove Linhas 1 do texto da serial
+    vagas = line_value.replace('Linha 1: ', '').strip()
+    # remove Linhas 1 do texto da serial
+    vagas = vagas.replace('Linha 2: ', '').strip()
+    # remove ultima virgula do texto se ouver
+    vagas = vagas.rstrip(',')
+    # transforma o texto em uma lista de vagas
+    vagas = vagas.split(',')
+    # mostra a lista que vai ser alterada
+    # print vagas
     for i in vagas:
+        # pega uma vaga
         vaga = i[:2]
+        # pega um status de uma vaga
         status = i[-1]
+        # altera a tabela do banco de dados
         con.execute('UPDATE vagas set status=? where vaga=?', (status, vaga))
-    con.commit()
+        # salva as modificações
+        con.commit()
 
-
+# loop infinito
 while True:
+    # le uma linha da serial
     message = ser.readline()
+    # se exister mensagem altera a tabela de status
     if message:
+        # mostra a mensagem recebida pela serial
+        print message
+        # altera o status da tabela
         update_content(con, message)
-        time.sleep(30)
-    print(message)
+        # aguarda 1 segundo para a proxima atualizacao
+        time.sleep(1)
+
