eabd3f68fb26b9ef8f8922861ee83cbff0330013
Valder <valdergallo@gmail.com>
2013-10-15 09:45:46 -0300

    merge

--- runserver.py
+++ runserver.py
@@ -22,16 +22,27 @@ def send_static(filename):
 def home(db, vaga=None):
     if not vaga:
         # mostra todas as vagas
-        c = db.execute('SELECT * FROM vagas')
+        query = db.execute('SELECT * FROM vagas')
     else:
         # mostra apenas uma vaga em especial
-        c = db.execute('SELECT * FROM vagas WHERE vaga = ?', (vaga,))
+        query = db.execute('SELECT * FROM vagas WHERE vaga = ?', (vaga,))
 
     # transforma o conteudo SQL em uma lista de dados
-    rows = c.fetchall()
+    rows = query.fetchall()
+
+    # conta quantas vagas estão livres
+    query = db.execute('SELECT count(id) FROM vagas where status=0')
+    vagas_ocupadas = query.fetchone()
+
+    # conta o total de vagas
+    query = db.execute('SELECT count(id) FROM vagas')
+    vagas_total = query.fetchone()
+
+    # monta mesagem padrão para ser exibida no template
+    mensagem = "{0} vagas livres de {1} vagas".format(vagas_ocupadas[0], vagas_total[0])
 
     # envia os dados para o template
-    return template('vagas_bo', vagas=rows)
+    return template('vagas_bo', vagas=rows, vagas_mensagem=mensagem)
 
+# inicia o webserver do bottle com debug na porta 8080
 run(host='0.0.0.0', port=8080, debug=True)
-
